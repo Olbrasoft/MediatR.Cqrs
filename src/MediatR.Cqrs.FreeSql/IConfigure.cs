7ba0c49c8218ff22354e92886c0728c81cfe021a
Jirka <tuma.rsrobot@gmailcom>
2024-08-08 20:03:53 +0200

    Add FreeSql support and update project configurations
    Updated the solution file to include new FreeSql projects and adjusted configurations. Added `MediatR.Cqrs.FreeSql` and `MediatR.Cqrs.FreeSql.Tests` projects. Modified `MediatR.Cqrs.Common.csproj` and `MediatR.Cqrs.EntityFrameworkCore.csproj` for package references and formatting. Introduced `DbRequestHandler` abstract class and `IConfiguration` and `IConfigure` interfaces in `MediatR.Cqrs.FreeSql`. Created new project and test project files targeting .NET 8.0 with necessary references. Added a placeholder test class in `MediatR.Cqrs.FreeSql.Tests`.

---
+++ src/MediatR.Cqrs.FreeSql/IConfigure.cs
@@ -0,0 +1,7 @@
+﻿using System.Linq.Expressions;
+
+namespace MediatR.Cqrs.FreeSql;
+public interface IConfigure<TSource>
+{
+    Expression<Func<TSource, TDestination>> Configure<TDestination>() where TDestination : new();
+}
---
+++ test/MediatR.Cqrs.FreeSql.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace MediatR.Cqrs.FreeSql.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
